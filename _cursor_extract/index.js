8bb0cad71137a4214240fde6b23b13eea3d748d1
uKruel <29132359+uKruel@users.noreply.github.com>
2021-02-22 12:31:22 +0000

    Add multi-tooltip functionality

--- _cursor_extract/index.js
+++ _cursor_extract/index.js
@@ -4,19 +4,59 @@ const pp = (item) => {
   console.log(item);
 };
 
+class TooltipUtils {
+  static idNum = 0;
+  static lastTooltipId = "";
+  static lockTooltip = true;
+  static legalInput = ["id", "class"];
+
+  static idsList = [];
+
+  static instantiateTooltip() {
+    const newTT = document.createElement("div");
+    this.lastTooltipId = "ctts" + this.idNum.toString();
+    this.idsList.push(this.lastTooltipId);
+    this.idNum++;
+    newTT.id = this.lastTooltipId;
+    newTT.className = "ctts";
+    document.body.appendChild(newTT);
+    return document.getElementById(this.lastTooltipId);
+  }
+
+  static destroyTooltip(tooltipID) {
+    document.getElementById(tooltipID).remove();
+  }
+}
+
 class MouseTooltip {
   constructor() {
-    this.lockTooltip = true;
+    this.lockTooltip = TooltipUtils.lockTooltip;
     [this.cursorX, this.cursorY] = [0, 0];
     this.allAttr = window.getComputedStyle(document.body);
 
-    this.tooltip = document.getElementById("ctooltip");
+    this.tooltip = TooltipUtils.instantiateTooltip();
+
     this.copyText = "";
 
     this.txtHtmHeader = "";
     this.txtToDisplay = "";
 
-    this.legalCSSInput = ["id", "class"];
+    document.addEventListener("keydown", (e) => {
+      //doesn't work, lockTooltip var is not being accessed properly
+      if (e.defaultPrevented) {
+        return;
+      }
+
+      switch (e.code) {
+        case "KeyS":
+        case "ArrowDown":
+          this.lockTooltip = !this.lockTooltip;
+          this.tooltip = TooltipUtils.instantiateTooltip();
+          break;
+        default:
+          break;
+      }
+    });
 
     document.addEventListener("mousemove", (e) => {
       if (this.lockTooltip) {
@@ -28,24 +68,14 @@ class MouseTooltip {
     });
 
     document.addEventListener("mousedown", (e) => {
-      //TODO, how to copy???
-
-      //pp(document.elementFromPoint(this.cursorX,this.cursorY));
-
+      //copy css
       navigator.clipboard.writeText(this.txtToDisplay);
-
       this.tooltip.style.backgroundColor = "#c8e6c9";
-      document.getElementById("ctooltip").innerHTML = "copied";
+      this.tooltip.innerHTML = "copied";
       const timerez = (duration) => {
         let seconds = duration;
         setInterval(function () {
-          //seconds = parseInt(timer % 60, 10);
-
-          //seconds = seconds < 10 ? "0" + seconds : seconds;
-
-          document.getElementById("ctooltip").style.backgroundColor =
-            "lightgray";
-
+          this.tooltip.style.backgroundColor = "lightgray";
           if (--seconds < 0) {
             return;
           }
@@ -53,7 +83,13 @@ class MouseTooltip {
       };
 
       timerez(1);
+      clearInterval(timerez);
     });
+
+    setInterval(() => {
+      this.displayCSSData_test();
+      //th.cssk();
+    }, 300);
   }
 
   tooltipFollow() {
@@ -67,14 +103,13 @@ class MouseTooltip {
   extractCSS(byAttribute, elementToView) {
     if (
       byAttribute === false ||
-      
       elementToView === null ||
       elementToView === "undefined"
     ) {
       return "";
     }
     let outputTxt = "";
-    for (const it of this.legalCSSInput) {
+    for (const it of TooltipUtils.legalInput) {
       if (it === byAttribute) {
         if (elementToView.getAttribute(it)) {
           for (let i = 0; i < document.styleSheets[0].cssRules.length; i++) {
@@ -138,26 +173,17 @@ class MouseTooltip {
       );
     }
 
-    this.tooltip.innerHTML = this.extractCSS("class",pointOfCursorFocus);
+    this.txtToDisplay =
+      pointOfCursorFocus.tagName.toLocaleLowerCase() +
+      "\n" +
+      this.prettyTextFormat(this.extractCSS("class", pointOfCursorFocus));
+    this.tooltip.innerHTML = this.txtToDisplay;
 
     //formatTooltipText(ss.cssRules[1].cssText);
     //pp(document.querySelector("#content3").getAttribute("style"));
-  }
-
-  
-
-  toggleLockTooltip() {
-    this.lockTooltip = !this.lockTooltip;
   }
 
   editCSS() {}
 }
 
 const th = new MouseTooltip();
-
-//th.extractCSS();
-
-setInterval(() => {
-  th.displayCSSData_test();
-  //th.cssk();
-}, 300);
